9bcdae3197c6fec4db711d7cde1613d595c4c85d
robel16 <99750963+robel16@users.noreply.github.com>
2024-06-18 02:55:48 +0300

    putting failed test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
+  @Test
+  public void intentionally_failed() {
+    assertTrue(false);
+  }  
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
